e96fc52f9824a665f23aa7bba6604b731a42cb69
Leal <italoleallanasantos@gmail.com>
2025-01-09 11:43:18 -0300

    add vpl8

--- vpl8/src/main.cpp
+++ vpl8/src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Ponto {
@@ -10,7 +12,7 @@ public:
     Ponto(int x, int y) {
         this->x = x;
         this->y = y;
-        cout << "Ponto " << this << " Criado" << endl;
+        // cout << "Ponto " << this << " Criado" << endl;
     };
 
     Ponto(int x) : Ponto(x, x) {
@@ -20,7 +22,7 @@ public:
     };
 
     ~Ponto() {
-        cout << "Ponto " << this << " Destruido" << endl;
+        // cout << "Ponto " << this << " Destruido" << endl;
     };
 
     void set_xy(int x, int y) {
@@ -42,58 +44,135 @@ public:
 };
 
 class FiguraGeometrica {
+protected:
+    Ponto centro;
+
+public:
+    FiguraGeometrica(int x, int y) {
+        centro = Ponto(x, y);
+        // centro.set_xy(x, y);
+    };
+
+    virtual ~FiguraGeometrica() {
+    };
+
+    virtual void desenha() {
+        cout << centro.get_x() << " " << centro.get_y() << " ";
+    };
+
+    virtual float calcular_area() = 0;
 };
 
-class Retangulo : FiguraGeometrica {
+class Retangulo : public FiguraGeometrica {
+private:
+    int lado1;
+    int lado2;
+
+public:
+    Retangulo(int x, int y, int lado1, int lado2) : FiguraGeometrica(x, y), lado1(lado1), lado2(lado2) {
+    }
+
+    void set_lados(int l1, int l2) {
+        this->lado1 = l1;
+        this->lado2 = l2;
+    };
+
+    void desenha() {
+        FiguraGeometrica::desenha();
+        cout << "RETANGULO" << endl;
+    };
+
+    float calcular_area() {
+        return (this->lado1) * (this->lado2);
+    };
 };
 
-class Triangulo : FiguraGeometrica {
+class Circulo : public FiguraGeometrica {
+private:
+    int raio;
+
+public:
+    Circulo(int x, int y, int raio) : FiguraGeometrica(x, y), raio(raio) {
+    }
+
+    void set_raio(int raio) {
+        this->raio = raio;
+    };
+
+    void desenha() {
+        FiguraGeometrica::desenha();
+        cout << "CIRCULO" << endl;
+    };
+
+    float calcular_area() {
+        return 3.141592653589 * (this->raio) * (this->raio);
+    };
 };
 
-class Circulo : FiguraGeometrica {
+class Triangulo : public FiguraGeometrica {
+private:
+    int base;
+    int altura;
+
+public:
+    Triangulo(int x, int y, int lado1, int lado2) : FiguraGeometrica(x, y), base(lado1), altura(lado2) {
+    }
+
+    void set_base_altura(int base, int altura) {
+        this->base = base;
+        this->altura = altura;
+    };
+
+    void desenha() {
+        FiguraGeometrica::desenha();
+        cout << "TRIANGULO" << endl;
+    };
+
+    float calcular_area() {
+        return (this->base) * (this->altura) / 2;
+    };
 };
 
 int main() {
-    cout << "Hello World" << endl;
-    Ponto A(10, 20);
-    Ponto B(-5);
-    Ponto C{};
-    A.print_xy();
-    B.print_xy();
-    C.print_xy();
-    return 0;
+    string entrada;
+    int cord1, cord2, l1, l2;
+    vector<FiguraGeometrica *> vetor_FigGeo;
+
+    while (true) {
+        cin >> entrada;
+
+        if (entrada == "R") {
+            cin >> cord1 >> cord2 >> l1 >> l2;
+            // R <C1> <C2> <L1> <L2>: Cria um retângulo com lados L1 e L2 com centro em C1, C2.
+
+            vetor_FigGeo.push_back(new Retangulo(cord1, cord2, l1, l2));
+        } else if (entrada == "C") {
+            cin >> cord1 >> cord2 >> l1; // C <C1> <C2> <R>: Cria um círculo de raio R com centro em C1, C2.
+            vetor_FigGeo.push_back(new Circulo(cord1, cord2, l1));
+        } else if (entrada == "T") {
+            cin >> cord1 >> cord2 >> l1 >> l2;
+            // T <C1> <C2> <B> <A>: Cria um triângulo de base B e altura A com centro em C1, C2.
+            vetor_FigGeo.push_back(new Triangulo(cord1, cord2, l1, l2));
+        } else if (entrada == "D") {
+            for (auto &figura: vetor_FigGeo) {
+                figura->desenha();
+                // D:  "Desenha" todas, imprimindo as coordenadas do seu centro e o texto especificado para cada tipo.
+            };
+        } else if (entrada == "A") {
+            float soma = 0; // A: calcula e imprime a área total ocupada por todas as figuras geométricas.
+            for (FiguraGeometrica * &figura: vetor_FigGeo) {
+                soma += figura->calcular_area();
+            };
+            cout << "AREA TOTAL: " << soma << endl;
+        } else if (entrada == "E") {
+            cout << "EXIT" << endl;
+            for (FiguraGeometrica* &figura: vetor_FigGeo) {
+                delete figura;
+            };
+            vetor_FigGeo.clear();
+            return 0;
+        } else {
+            cout << "ERRO: Digite uma das letras a seguir: R, C, T, D, A, E" << endl;
+        };
+    };
 };
-/*
-A sua superclasse deve ser uma classe abstrata.
-Ela possui um atributo chamado "centro" com coordenadas do centro da figura geométrica.
-Esse atributo deve ser um objeto de outra classe chamada "Ponto",
-criada para representar um ponto no espaço 2D (implemente a classe
-com construtores adequados e métodos Get / Set para os atributos.
-A sua classe base também deve possuir e um mét odo chamado
-"Desenha" que imprime essas coordenadas e que pode ser redefinido
-nas classes herdeiras. Além disso, a sua classe possui um mét odo virtual
-puro chamado "CalculaArea" que calcula a área ocupada pelas figuras geométricas
-(retornando um valor float) e será implementado nas classes herdeiras.
-A classe Retângulo tem como os atributos os dois lados do retângulo.
-O seu mét odo Desenha chama o mét odo da classe pai e depois imprime a palavra
-"RETANGULO". Já o seu mét odo CalculaArea retorna a área do retângulo.
-A classe Círculo tem como o atributo o raio do círculo
-. O seu mét odo Desenha chama o mét odo da classe pai e depois imprime a palavra
-"CIRCULO", e o seu mét odo CalculaArea retorna a área do circulo.
-Por fim, a classe Triângulo tem como atributos a base e altura do triangulo.
-O seu mét odo Desenha chama o mét odo da classe pai e depois imprime a palavra
-"TRIANGULO", e o seu mét odo CalculaArea retorna a sua área.
-Use adequadamente os modificadores de acesso (public, protected e private)
-em suas classes e implemente os métodos construtores e destrutores conforme necessário.
-O seu programa principal deverá armazenar um vetor de figuras geométricas
-e receber comandos para a sua manipulação. Mais especificamente, ele vai ser um
-loop que lê comandos da entrada (R, C, T, D, A, E) juntamente com eventuais
-parâmetros e faz as operações de acordo.  Os comandos são:
-R <C1> <C2> <L1> <L2>: Cria um retângulo com lados L1 e L2 com centro em C1, C2.
-C <C1> <C2> <R>: Cria um círculo de raio R com centro em C1, C2.
-T <C1> <C2> <B> <A>: Cria um triângulo de base B e altura A com centro em C1, C2.
-D:  "Desenha" todas as figuras geométricas armazenadas, imprimindo as
-coordenadas do seu centro e o texto especificado para cada tipo.
-A: calcula e imprime a área total ocupada por todas as figuras geométricas.
-E: termina a execução do programa.
-*/
